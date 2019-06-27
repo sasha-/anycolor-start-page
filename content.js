ab35f46ae39a82989770c1846b9782152570ddac
sasha- <sasha-@users.noreply.github.com>
2019-06-27 16:11:51 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded',
-    function(){
-        color = chrome.storage.sync.get("color",
-            function (color){
-                document.getElementsByTagName("body")[0].style.backgroundColor = color['color'];
-            }
-        );
+chrome.storage.sync.get("color",
+    function (color){
+        document.getElementsByTagName("body")[0].style.backgroundColor = color['color'];
     }
 );
